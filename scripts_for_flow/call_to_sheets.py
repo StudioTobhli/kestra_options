2d8c35ebb25ef8c22fe0ca811a7bf3e76e5baefb
tobhli_token <studio.toby.lee@gmail.com>
2026-03-10 05:36:33 +0000

    Changed put_to_sheet name, added stock hisstory to call_to_sheets

--- scripts_for_flow/call_to_sheets.py
+++ scripts_for_flow/call_to_sheets.py
@@ -19,3 +19,24 @@ wksht.clear()
 set_with_dataframe(wksht, call_leads_df, include_index=False, include_column_header=True, resize=True)
 
 print(f"Wrote {len(call_leads_df)} rows to Google Sheets")
+
+# Write stock history of call data
+call_stock_hist_qry = """
+SELECT sh.ticker
+     , sh.hist_date
+     , sh.close
+  FROM stock_hist_data sh
+  JOIN current_holdings ch
+  ON sh.ticker = ch.ticker
+"""
+
+call_stock_hist_df = pd.read_sql(call_stock_hist_qry, engine)
+
+# Open target gsheet
+hist_sh = gc.open("call_stock_hist_data")
+hist_ws = hist_sh.get_worksheet(0)
+
+# Write stock history to gsheet
+set_with_dataframe(hist_ws, call_stock_hist_df, include_index=False, include_column_header=True, resize=True)
+
+print(f"Wrote {len(call_stock_hist_df)} rows to call_stock_hist_data")
